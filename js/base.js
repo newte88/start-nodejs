61d2bf0a04c2075910bc262b764b15fe16a5c038
zhanhongtao <redky@live.com>
2012-12-29 14:41:37 +0800

    JavaScript 部分兼容 console 对象

--- js/base.js
+++ js/base.js
@@ -2,6 +2,20 @@
 
 
 
+;(function () {
+    var noop = function () {};
+    if ( !window.console ) {
+        window.console = {
+            log: noop,
+            debug: noop,
+            warn: noop,
+            error: noop,
+            dir: noop,
+            assert: noop,
+            info: noop
+        };
+    }
+})();
 
 console.log( 'init' );
 
